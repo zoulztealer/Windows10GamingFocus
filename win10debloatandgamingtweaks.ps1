f2d41ab828256b4270772d895808172e6beb2801
daddymadu <9450141+DaddyMadu@users.noreply.github.com>
2024-01-08 23:09:51 +0200

    resetting bcdedit for hyper-v

--- win10debloatandgamingtweaks.ps1
+++ win10debloatandgamingtweaks.ps1
@@ -3019,9 +3019,11 @@ Function DisableDMA {
         Write-Output "Disabling DMA memory protection and cores isolation..."
 	$errpref = $ErrorActionPreference #save actual preference
         $ErrorActionPreference = "silentlycontinue"
-        bcdedit /set vsmlaunchtype Off | Out-Null
-        bcdedit /set vm No | Out-Null
-	bcdedit /set loadoptions DISABLE-LSA-ISO,DISABLE-VBS | Out-Null
+        #bcdedit /set vsmlaunchtype Off | Out-Null
+        #bcdedit /set vm No | Out-Null
+		bcdedit /deletevalue vsmlaunchtype | Out-Null
+		bcdedit /deletevalue vm | Out-Null
+		bcdedit /set loadoptions DISABLE-LSA-ISO,DISABLE-VBS | Out-Null
         New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\FVE" | Out-Null -ErrorAction SilentlyContinue
         Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\FVE" -Name "DisableExternalDMAUnderLock" -Type DWord -Value 0
         New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeviceGuard" | Out-Null -ErrorAction SilentlyContinue
